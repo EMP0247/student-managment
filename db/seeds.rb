230c7e68c93573ea4fa1d32d14dd7db7b1b6a064
EMP0247 <hutish@vebuin.com>
2023-06-19 14:43:32 +0530

    updated commit

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+student = Student.create(
+    first_name: 'Hutish',
+    last_name: 'Seju',
+    email: 'sejuhutish@gmail.com'
+)
+
+10.times do |i|
+    Student.create(
+        first_name: "Student-",
+        last_name: "#{i+1}",
+        email: "student#{i+1}@demo.com"
+    )
+end
